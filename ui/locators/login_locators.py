8c7cf9f1ebe6910e0ce0fe403f219b86d1abd23f
Talgat <gumirovt@mail.ru>
2025-11-19 21:52:09 +0100

    add negative login tests

--- ui/locators/login_locators.py
+++ ui/locators/login_locators.py
@@ -4,3 +4,4 @@ class LoginLocators:
     LOGIN = (By.CSS_SELECTOR, '[data-test="username"]')
     PASS = (By.CSS_SELECTOR, '[data-test="password"]')
     BUTTON = (By.CSS_SELECTOR, '[data-test="login-button"]')
+    ERROR_MESSAGE = (By.CSS_SELECTOR, '[data-test="error"]')
--- ui/tests/test_login.py
+++ ui/tests/test_login.py
@@ -1,4 +1,7 @@
+import pytest
+
 from ui.pages.login_page import LoginPage
+from ui.locators.login_locators import LoginLocators
 
 base = 'https://www.saucedemo.com/'
 
@@ -8,3 +11,14 @@ def test_user_can_login(browser):
     page.open()
     page.login('standard_user', 'secret_sauce')
     assert page.browser.current_url != base, 'You are not logged in'
+
+
+@pytest.mark.parametrize('login, password, error', [('standard_user','wrongpass', "Epic sadface: Username and password do not match any user in this service"),
+                                                    ('','secret_sauce', "Epic sadface: Username is required"),
+                                                    ('standard_user','', "Epic sadface: Password is required"),
+                                                    ('locked_out_user','secret_sauce', 'Epic sadface: Sorry, this user has been locked out.')])
+def test_login_invalid_data(browser, login, password, error):
+    page = LoginPage(browser, base)
+    page.open()
+    page.login(login, password)
+    assert page.browser.find_element(*LoginLocators.ERROR_MESSAGE).text == error
